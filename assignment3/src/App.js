edf054d9580f9b94bf268b67d575928e2ec4a6c9
Kaley Du <kaley.du1@gmail.com>
2024-03-11 19:59:10 -0400

    in react

--- assignment3/src/App.js
+++ assignment3/src/App.js
@@ -15,6 +15,11 @@ import csv_1 from "./graphs/random_points_color_1.csv";
 import csv_2 from "./graphs/random_points_color_2.csv";
 import csv_3 from "./graphs/random_points_color_3.csv";
 import csv_4 from "./graphs/random_points_color_4.csv";
+import csv_2_0 from "./graphs/experiment2_random_points_saturated.csv"
+import csv_2_1 from "./graphs/experiment2_random_points_second_farthest.csv"
+import csv_2_2 from "./graphs/experiment2_random_points_light.csv"
+import csv_2_3 from "./graphs/experiment2_random_points_muted.csv"
+import csv_2_4 from "./graphs/experiment2_random_points_dark.csv"
 
 
 var timeStart
@@ -27,6 +32,11 @@ function App() {
     csv_2,
     csv_3,
     csv_4,
+    csv_2_0,
+    csv_2_1,
+    csv_2_2,
+    csv_2_3,
+    csv_2_4,
   ];
 
   const [csvNumber, setcsvNumber] = useState(0);
@@ -60,28 +70,36 @@ function App() {
   function navGraph(event){
     document.getElementById("graph").style.display = "block";
     document.getElementById("intro").style.display = "none";
+    document.getElementById("experiment2").style.display = "none";
     document.getElementById("break").style.display = "none";
     timeStart = performance.now()
   }
 
   function navBreak(event){
     timeEnd = performance.now()
-    if(csvNumber == 4){
+    if(csvNumber === 4){
+      document.getElementById("graph").style.display = "none";
+      document.getElementById("experiment2").style.display = "block";
+    }
+    else if(csvNumber === 9){
       document.getElementById("graph").style.display = "none";
       document.getElementById("end").style.display = "block";
+      document.getElementById("experiment2").style.display = "none";
     }
     else{
       document.getElementById("graph").style.display = "none";
       document.getElementById("break").style.display = "block";
+      document.getElementById("experiment2").style.display = "none";
     }
     console.log(timeEnd-timeStart) //Prints time to click
     setcsvNumber(n => n+1)
   }
 
   useEffect(() => {
-    if(csvNumber == 0){
+    if(csvNumber === 0){
       document.getElementById("break").style.display = "none";
       document.getElementById("graph").style.display = "none";
+      document.getElementById("experiment2").style.display = "none";
       document.getElementById("end").style.display = "none";
       }
   }, []);
@@ -107,6 +125,12 @@ function App() {
 
       <div id="break">
         <h3 className="mx-auto p-2">Click "next" when you're ready for the next graph.</h3>
+        <button className="btn btn-primary" onClick={navGraph}>Next</button> 
+      </div>
+
+      <div id= "experiment2">
+        <h2>Experiment 2</h2>
+        <h3 className="mx-auto p-2">In this experiment, you are asked to find and click the black circle as fast as you can in a scatterplot. There will be 5 scatterplots in the experiment. We won't record any other information from you except the time it takes to find and click the black circle.</h3>
         <button className="btn btn-primary" onClick={navGraph}>Next</button> 
       </div>
 
